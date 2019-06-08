1d0028366d37a82fe995b659516820165a5156e4
vitorcarminatti <vitor.carminatti@hotmail.com>
2019-06-08 18:56:26 -0300

    Alterações

--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -1,7 +1,57 @@
 import React from "react";
-import { DatePicker } from "antd";
-import "antd/dist/antd.css";
+import { Form, Icon, Input, Button, Checkbox } from "antd";
+import { LoginWrapper, LinksWrapper } from "./styles";
 
-const Login = () => <DatePicker />;
+class Login extends React.Component {
+  state = {
+    email: "",
+    senha: ""
+  };
 
-export default Login;
+  handleSubmit = e => {
+    console.log(e);
+  };
+
+  render() {
+    return (
+      <LoginWrapper>
+        <h1>Login</h1>
+        <Form onSubmit={this.handleSubmit} className="login-form">
+          <Form.Item>
+            <Input
+              prefix={<Icon type="user" style={{ color: "rgba(0,0,0,.25)" }} />}
+              placeholder="Email"
+              onChange={() => this.handleChange(this)}
+            />
+          </Form.Item>
+          <Form.Item>
+            <Input
+              prefix={<Icon type="lock" style={{ color: "rgba(0,0,0,.25)" }} />}
+              type="password"
+              placeholder="Senha"
+              onChange={() => this.handleChange()}
+            />
+          </Form.Item>
+          <Form.Item>
+            <LinksWrapper>
+              <Checkbox>Lembre-me</Checkbox>
+              <a className="login-form-forgot" href="">
+                Esqueceu sua senha?
+              </a>
+              <Button
+                type="primary"
+                htmlType="submit"
+                className="login-form-button"
+              >
+                Entrar
+              </Button>
+              <a href="">Cadastre-se</a>
+            </LinksWrapper>
+          </Form.Item>
+        </Form>
+      </LoginWrapper>
+    );
+  }
+}
+
+export default Form.create()(Login);
