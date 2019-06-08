e2eb48792f2c6e6091cde505da7431b91cc05533
Carlotto <carlotto.dias@gmail.com>
2019-06-08 18:58:40 -0300

    commit02

--- src/App.js
+++ src/App.js
@@ -2,19 +2,13 @@ import React from "react";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import Charts from "./pages/charts";
 import Login from "./pages/login";
+import Nav from "./components/menu";
 
 function App() {
   return (
     <Router>
       <div>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/charts">Charts</Link>
-            </li>
-          </ul>
-        </nav>
-
+        <Nav />
         <Route path="/charts" exact component={Charts} />
         <Route path="/login" exact component={Login} />
       </div>
