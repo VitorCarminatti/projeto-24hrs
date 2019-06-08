2cf4ca2416ceac2d52e2c8086bed0e1059ca2279
Carlotto <carlotto.dias@gmail.com>
2019-06-08 16:55:40 -0300

    Merge01

--- src/pages/charts/index.js
+++ src/pages/charts/index.js
@@ -1,5 +1,35 @@
 import React from "react";
+import Chart from "react-google-charts";
 
-const Charts = () => <h1>Tela de gráficos</h1>;
+const Charts = () => (
+  <div style={{ display: "flex", maxWidth: 900 }}>
+    <Chart
+      width={300}
+      height={300}
+      chartType="ColumnChart"
+      loader={<div>Loading Chart</div>}
+      data={[
+        ["City", "2010 Population", "2000 Population"],
+        ["New York City, NY", 8175000, 8008000],
+        ["Los Angeles, CA", 3792000, 3694000],
+        ["Chicago, IL", 2695000, 2896000],
+        ["Houston, TX", 2099000, 1953000],
+        ["Philadelphia, PA", 1526000, 1517000]
+      ]}
+      options={{
+        title: "Population of Largest U.S. Cities",
+        chartArea: { width: "30%" },
+        hAxis: {
+          title: "Total Population",
+          minValue: 0
+        },
+        vAxis: {
+          title: "City"
+        }
+      }}
+      legendToggle
+    />
+  </div>
+);
 
 export default Charts;
