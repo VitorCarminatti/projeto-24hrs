b87ffa31e30a73e701d1c1daae19aa7ce408f3d8
vitorcarminatti <vitor.carminatti@hotmail.com>
2019-06-08 15:39:44 -0300

    Favicon e router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import React from "react";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import Charts from "./pages/charts";
 
 function App() {
   return (
-    <div>
-      <h1>Testelha</h1>
-    </div>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/charts">Charts</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <Route path="/charts" exact component={Charts} />
+      </div>
+    </Router>
   );
 }
 
---
+++ src/pages/charts/index.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+const Charts = () => <h1>Tela de gráficos</h1>;
+
+export default Charts;
